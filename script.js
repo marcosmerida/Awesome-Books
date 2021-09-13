f346c1ad8aca3c97100c0da92b3489aa834454d5
marcosmerida <marcosmerida@gmail.com>
2021-09-13 09:18:02 -0600

    variables and  main function of the project

---
+++ script.js
@@ -0,0 +1,10 @@
+let booklist = document.getElementById("books");
+let titleinput = document.getElementById("title");
+let authorinput = document.getElementById("author");
+let submitbutton = document.getElementById("button");
+
+function Addbook(){
+    let booktitle = titleinput.textContent;
+    let bookauthor = authorinput.textContent;
+    let book = "<li></li>"
+}
