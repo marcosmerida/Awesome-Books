0471c0f4d2c58f40076b3aa0474ea56192ebd73b
marcosmerida <marcosmerida@gmail.com>
2021-09-13 13:58:10 -0600

    doble quote for strings removed

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 /* eslint-disable no-unused-vars */
 
-const booklist = document.getElementById("books");
+const booklist = document.getElementById('books');
 let BookArray = [];
 
 function printScreen() {
-  BookArray = JSON.parse(localStorage.getItem("data"));
+  BookArray = JSON.parse(localStorage.getItem('data'));
   if (BookArray == null) {
     BookArray = [];
   }
-  booklist.innerHTML = "";
+  booklist.innerHTML = '';
   BookArray.forEach((value, index) => {
     const book = `<li> <p> ${value[0]}</p> <p>${value[1]}</p><button onclick="removeBookFroList(${index})">Remove</button><hr></li>`;
-    const liTag = document.createElement("li");
-    liTag.setAttribute("id", index);
+    const liTag = document.createElement('li');
+    liTag.setAttribute('id', index);
     liTag.innerHTML = book.trim();
     booklist.appendChild(liTag);
   });
 }
 
 function updateBookStorage() {
-  localStorage.setItem("data", JSON.stringify(BookArray));
+  localStorage.setItem('data', JSON.stringify(BookArray));
 }
 
 function addBookToList() {
-  const titleinput = document.getElementById("title").value;
-  const authorinput = document.getElementById("author").value;
+  const titleinput = document.getElementById('title').value;
+  const authorinput = document.getElementById('author').value;
   BookArray.push([titleinput, authorinput]);
   updateBookStorage();
   printScreen();
