44d4603adb1cdebc025524b34d0d0f78cb1459e1
marcosmerida <marcosmerida@gmail.com>
2021-09-13 13:13:21 -0600

    Spacing errors about linters fixed

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const booklist = document.getElementById('books');
 let BookArray = [];
 
-function printScreen(){
+function printScreen() {
   BookArray = JSON.parse(localStorage.getItem('data'));
-  if(BookArray == null){
-      BookArray = [];
+  if (BookArray == null) {
+    BookArray = [];
   }
-  booklist.innerHTML = "";
+  booklist.innerHTML = '';
   BookArray.forEach(function(value, index) {
-      let book = '<li> <p>' + value[0] + '</p> <p>' + 
-  value[1] + '</p>'+ 
-  '<button onclick="removeBookFroList('+index+')">Remove</button><hr></li>';
-  let liTag = document.createElement('li');
-  liTag.setAttribute('id', index + ''); 
-  liTag.innerHTML = book.trim();
-  booklist.appendChild(liTag);
-    });
-  }
+      const book = '<li> <p>' + value[0] + '</p> <p>' 
+      + value[1] + '</p>'
+      + '<button onclick="removeBookFroList(' + index + ')">Remove</button><hr></li>';
+    const liTag = document.createElement('li');
+    liTag.setAttribute('id', index);
+    liTag.innerHTML = book.trim();
+    booklist.appendChild(liTag);
+  });
+}
 
 function addBookToList(){
   const titleinput = document.getElementById('title').value;
