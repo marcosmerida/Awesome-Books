23b9fa7435c723fe9cfd06935dc485859c4e3e99
marcosmerida <marcosmerida@gmail.com>
2021-09-13 12:32:23 -0600

    Linters errors has been fixed

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ function addBookToList(){
     let titleinput = document.getElementById('title').value;
     let authorinput = document.getElementById('author').value;
     BookArray.push([titleinput, authorinput]);
-    updateBookStorage()
-    printScreen()
-}
-
-
-function removeBookFroList(index){
-    BookArray.splice(index,1);
-    updateBookStorage()
+    updateBookStorage();
     printScreen();
 }
 
+function removeBookFroList(index){
+    BookArray.splice(index,1);
+    updateBookStorage();
+    printScreen();
+}
 
 function printScreen(){
     BookArray = JSON.parse(localStorage.getItem('data'));
-    booklist.innerHTML = '';
+    booklist.innerHTML = "";
     BookArray.forEach(function(value, index){
         let book = '<li> <p>' + value[0] + '</p> <p>' + 
     value[1] + '</p>'+ 
@@ -27,12 +25,12 @@ function printScreen(){
     let liTag = document.createElement('li');
     liTag.setAttribute('id', index + ''); 
     liTag.innerHTML = book.trim();
-    booklist.appendChild(liTag)
-    })
+    booklist.appendChild(liTag);
+    });
 }
 
 printScreen();
 
 function updateBookStorage(){
-    localStorage.setItem('data', JSON.stringify(BookArray))
+    localStorage.setItem('data', JSON.stringify(BookArray));
 }
