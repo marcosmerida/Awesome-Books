0d5c7395ac69552e09aca4a80675766dc0733dc2
ThankGod Richard <madueketf@gmail.com>
2021-09-13 16:52:42 +0100

    Adbook edited
    Addbook method adds book to ul list

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ let submitbutton = document.getElementById("button");
 function Addbook(){
     let booktitle = titleinput.textContent;
     let bookauthor = authorinput.textContent;
-    let book = "<li></li>"
+    let book = "<li> <p>${booktitle}</p> <p>${bookauthor}</p></li>"
+    let liTag = document.createElement('li');
+    liTag.innerHTML = book.trim();
+    booklist.appendChild(liTag)
 }
