1fb4db3f80b2bfada2c47db02f65b2ad0ebe02cc
marcosmerida <marcosmerida@gmail.com>
2021-09-13 12:53:51 -0600

    Fixing linter errors

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const booklist = document.getElementById('books');
 let BookArray = [];
 
+function printScreen(){
+  BookArray = JSON.parse(localStorage.getItem('data'));
+  if(BookArray == null){
+      BookArray = [];
+  }
+  booklist.innerHTML = "";
+  BookArray.forEach(function(value, index) {
+      let book = '<li> <p>' + value[0] + '</p> <p>' + 
+  value[1] + '</p>'+ 
+  '<button onclick="removeBookFroList('+index+')">Remove</button><hr></li>';
+  let liTag = document.createElement('li');
+  liTag.setAttribute('id', index + ''); 
+  liTag.innerHTML = book.trim();
+  booklist.appendChild(liTag);
+    });
+  }
+
 function addBookToList(){
-    let titleinput = document.getElementById('title').value;
-    let authorinput = document.getElementById('author').value;
-    BookArray.push([titleinput, authorinput]);
-    updateBookStorage();
-    printScreen();
+  const titleinput = document.getElementById('title').value;
+  const authorinput = document.getElementById('author').value;
+  BookArray.push([titleinput, authorinput]);
+  updateBookStorage();
+  printScreen();
 }
 
 function removeBookFroList(index){
-    BookArray.splice(index,1);
-    updateBookStorage();
-    printScreen();
-}
-
-function printScreen(){
-    BookArray = JSON.parse(localStorage.getItem('data'));
-    if(BookArray == null){
-        BookArray = [];
-    }
-    booklist.innerHTML = "";
-    BookArray.forEach(function(value, index) {
-        let book = '<li> <p>' + value[0] + '</p> <p>' + 
-    value[1] + '</p>'+ 
-    '<button onclick="removeBookFroList('+index+')">Remove</button><hr></li>';
-    let liTag = document.createElement('li');
-    liTag.setAttribute('id', index + ''); 
-    liTag.innerHTML = book.trim();
-    booklist.appendChild(liTag);
-    });
+  BookArray.splice(index,1);
+  updateBookStorage();
+  printScreen();
 }
 
 printScreen();
 
 function updateBookStorage(){
-    localStorage.setItem('data', JSON.stringify(BookArray));
+  localStorage.setItem('data', JSON.stringify(BookArray));
 }
