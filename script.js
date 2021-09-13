4d275b65c04be598dc5dbc48f6df1f84814a2a4f
marcosmerida <marcosmerida@gmail.com>
2021-09-13 10:00:58 -0600

    javascript file modified

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let booklist = document.getElementById("books");
-let titleinput = document.getElementById("title");
+let titleinput = document.getElementById("title").;
 let authorinput = document.getElementById("author");
 let submitbutton = document.getElementById("button");
 
 function Addbook(){
-    let booktitle = titleinput.textContent;
-    let bookauthor = authorinput.textContent;
-    let book = "<li> <p>${booktitle}</p> <p>${bookauthor}</p></li>"
+    let booktitle = titleinput.value;
+    let bookauthor = authorinput.innerHTML;
+    let book = '<li> <p>' + titleinput + '</p> <p>' + authorinput + '</p></li>';
     let liTag = document.createElement('li');
     liTag.innerHTML = book.trim();
     booklist.appendChild(liTag)
+    console.log(titleinput);
 }
