1d04cf9403288f37933a58b3bc3170b772eba708
ThankGod Richard <madueketf@gmail.com>
2021-09-13 17:19:18 +0100

    addbook edited
    Books now added to Views

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 let booklist = document.getElementById("books");
-let titleinput = document.getElementById("title").;
+let titleinput = document.getElementById("title");
 let authorinput = document.getElementById("author");
 let submitbutton = document.getElementById("button");
+let counter = 1;
 
 function Addbook(){
     let booktitle = titleinput.value;
-    let bookauthor = authorinput.innerHTML;
-    let book = '<li> <p>' + titleinput + '</p> <p>' + authorinput + '</p></li>';
+    let bookauthor = authorinput.value;
+
+
+    let book = '<li id="'+counter+'"> <p>' + booktitle + '</p> <p>' + 
+    bookauthor + '</p>'+ 
+    '<button onclick="removeBook('+counter+')">Remove</button><hr></li>';
     let liTag = document.createElement('li');
     liTag.innerHTML = book.trim();
     booklist.appendChild(liTag)
-    console.log(titleinput);
+    console.log(booktitle);
+    counter++;
+}
+function removeBook(id){
+    
 }
