6d908486b3529439a583ce91c427b57367d2d9f7
ThankGod Richard <madueketf@gmail.com>
2021-09-13 20:31:26 +0100

    arrow function fixed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function printScreen() {
     BookArray = [];
   }
   booklist.innerHTML = '';
-  BookArray.forEach(function (value, index) {
+  BookArray.forEach((value, index) => {
     const book = '<li> <p>' + value[0] + '</p> <p>'
       + value[1] + '</p>'
       + '<button onclick="removeBookFroList(' + index + ')">Remove</button><hr></li>';
