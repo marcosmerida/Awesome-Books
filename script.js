44778b644131d77706318d363dd56f565576a9ef
marcosmerida <marcosmerida@gmail.com>
2021-09-14 10:53:11 -0600

    Doble quotes eliminated for linters

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-/* eslint-disable no-unused-vars */
-
-/* const booklist = document.getElementById('books');
-let BookArray = [];
-
-
-function printScreen() {
-  BookArray = JSON.parse(localStorage.getItem('data'));
-  if (BookArray == null) {
-    BookArray = [];
-  }
-  booklist.innerHTML = '';
-  BookArray.forEach((value, index) => {
-    const book = `<li> <p> ${value[0]}</p> <p>${value[1]}</p><button onclick="removeBookFroList(${index})">Remove</button><hr></li>`;
-    const liTag = document.createElement('li');
-    liTag.setAttribute('id', index);
-    liTag.innerHTML = book.trim();
-    booklist.appendChild(liTag);
-  });
-}
-
-function updateBookStorage() {
-  localStorage.setItem('data', JSON.stringify(BookArray));
-}
-
-function addBookToList() {
-  const titleinput = document.getElementById('title').value;
-  const authorinput = document.getElementById('author').value;
-  BookArray.push([titleinput, authorinput]);
-  updateBookStorage();
-  printScreen();
-}
-
-function removeBookFroList(index) {
-  BookArray.splice(index, 1);
-  updateBookStorage();
-  printScreen();
-}
-
-printScreen(); */
-
 class Book {
   title;
   author;
@@ -64,32 +23,30 @@ class AwesomeBooks {
 
   removeBook(index) {
     this.allbooks.splice(index, 1);
-    console.log("removed called");
     this.#updateBookStorage();
   }
 
   #updateBookStorage() {
-    localStorage.setItem("data", JSON.stringify(this.allbooks));
+    localStorage.setItem('data', JSON.stringify(this.allbooks));
   }
 }
 
-const booklist = document.getElementById("books");
+const booklist = document.getElementById('books');
 let BookArray = [];
 let awesomeBooks;
 
 function printScreen() {
-  booklist.innerHTML = "";
-  bookArray = JSON.parse(localStorage.getItem("data"));
+  booklist.innerHTML = '';
+  bookArray = JSON.parse(localStorage.getItem('data'));
   if (BookArray == null) {
     BookArray = [];
   }
   awesomeBooks = new AwesomeBooks(bookArray);
-  booklist.innerHTML = "";
   if (awesomeBooks.allbooks.length > 0) {
     awesomeBooks.allbooks.forEach((book, index) => {
-      const bookone = `<li> <p> ${book.title}</p> <p>${book.author}</p><button onclick="removeBookFroList(${index})">Remove</button><hr></li>`;
-      const liTag = document.createElement("li");
-      liTag.setAttribute("id", index);
+      const bookone = `<li> <p> ${book.title}</p> <p>${book.author}</p><button onclick='removeBookFroList(${index})'>Remove</button><hr></li>`;
+      const liTag = document.createElement('li');
+      liTag.setAttribute('id', index);
       liTag.innerHTML = bookone.trim();
       booklist.appendChild(liTag);
     });
@@ -97,8 +54,8 @@ function printScreen() {
 }
 
 function addBookToList() {
-  const titleinput = document.getElementById("title").value;
-  const authorinput = document.getElementById("author").value;
+  const titleinput = document.getElementById('title').value;
+  const authorinput = document.getElementById('author').value;
   awesomeBooks.addBook(titleinput, authorinput);
   printScreen();
 }
