078949f5e9a39f117200558e4e74d11685633df9
marcosmerida <marcosmerida@gmail.com>
2021-09-14 11:21:27 -0600

    class.js added

---
+++ class.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+class Book {
+    title;
+  
+    author;
+  
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 /* eslint-disable no-unused-vars */
-class Book {
-  title;
-
-  author;
-
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
 class AwesomeBooks {
   allbooks = [];
 
