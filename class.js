4a75b3ac76363e0be2f40a5e153aa173699245e0
marcosmerida <marcosmerida@gmail.com>
2021-09-14 11:24:29 -0600

    indentation errors fixed

--- class.js
+++ class.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 class Book {
     title;
-  
+
     author;
-  
+
     constructor(title, author) {
       this.title = title;
       this.author = author;
     }
-  }
+}
